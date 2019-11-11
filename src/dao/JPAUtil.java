bacb039bef7df090c9d85bc3d09db9b46705a15e
Vitor <47614143+vitorleals@users.noreply.github.com>
2019-11-11 00:45:34 -0300

    ATUALIZEI TUDO

--- src/dao/JPAUtil.java
+++ src/dao/JPAUtil.java
@@ -1,40 +1,21 @@
 package dao;
 
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JPAUtil {
 
-	@Inject
-	private static PersistenceProperties properties;
+	// usar a estratégia SINGLETON
 
-	 private static EntityManagerFactory fabrica;
+		private JPAUtil() {
+			super();
+		}
+		
+		// criar a fábrica de EntityManager para o banco da aplicação
+		private static EntityManagerFactory fabrica = 
+							Persistence.createEntityManagerFactory("prontuni");
 
-	// static {
-	/// fabrica = Persistence.createEntityManagerFactory("prontuni");
-	// }
-
-	public JPAUtil() {
-		super();
-	}
-
-	static {
-        try {
-        	fabrica = Persistence.createEntityManagerFactory("prontuni",properties.get());
-             System.out.println("Entity Menager Test.............."+ fabrica);
-        } catch (Throwable ex) {
-
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-
-          }
-}
-	
-	
-	// criar a fábrica de EntityManager para o banco da aplicação
-	//private static EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("prontuni");
 
 	public static EntityManager getEntityManager() {
 		return fabrica.createEntityManager();
